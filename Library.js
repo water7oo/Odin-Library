aa8c34636b53a86f60d64f0ea1062f70915183ab
water7oo <watersevens37@gmail.com>
2023-10-25 21:45:44 -0400

    Can add/delete/clear books

--- Library.js
+++ Library.js
@@ -1,1 +1,42 @@
-console.log("Hello World");
+const bookArea = document.getElementById("bookArea");
+const bookTitle = document.getElementById("bookInput").value;
+const newParagraph = document.createElement("p");
+
+const myLibrary = [];
+
+function Book(title) {
+  this.title = title;
+}
+
+function addBooks() {
+  const bookTitle = document.getElementById("bookInput").value;
+  const newParagraph = document.createElement("p");
+  newParagraph.textContent = bookTitle;
+
+  bookArea.appendChild(newParagraph);
+
+  document.getElementById("bookInput").value = "";
+
+  const newBook = new Book(bookTitle);
+
+  myLibrary.push(newBook);
+
+  console.log(myLibrary);
+  event.preventDefault();
+}
+
+function deleteRecent() {
+  const lastChild = bookArea.lastChild;
+
+  if (lastChild) {
+    bookArea.removeChild(lastChild);
+  }
+
+  event.preventDefault();
+}
+function clearBooks() {
+  while (bookArea.firstChild) {
+    bookArea.removeChild(bookArea.firstChild);
+  }
+  event.preventDefault();
+}
